9e9f9041c8a31544216a2d9afcb1dacae793862f
Sung-Jaewoo <jaewoosung0407@gmail.com>
2026-04-08 10:18:17 +0900

    자기소개페이지 코드 첨부

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import ProfileCard from './ProfileCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      gap: '20px',
+      justifyContent: 'center',
+      marginTop: '60px',
+      flexWrap: 'wrap'
+    }}>
+      <ProfileCard name="성재우" age={21} dream="프론트엔드 개발자" emoji="🧑‍💻" hobby="기타 연주" mbti="ENFP" />
     </div>
   );
 }
